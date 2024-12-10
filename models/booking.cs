8b1d0a9178df334c73e25eeb803c31eedf66954b
budoor <budooralsaidi1998@gmail.com>
2024-12-10 16:31:16 +0400

    add models for all class

---
+++ models/booking.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanCllinicSystem.models
+{
+    [PrimaryKey(nameof(pid), nameof(cid), nameof(date))]
+    public class booking
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int bookid { get; set; }
+
+        [Required]
+
+        public DateTime date { get; set; }
+
+        [Required]
+        public int slot_number { get; set; }
+
+        [ForeignKey("patients")]
+        public int pid { get; set; }
+        public virtual Patient patients { get; set; }
+
+        [ForeignKey("clinics")]
+        public int cid { get; set; }
+        public virtual Clinic clinics { get; set; }
+
+    }
+}
